0c1df0bc51a0409d34a8be22045833f1397493b8
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2023-01-20 16:10:50 +0100

    creation database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Comic;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function home () {
-        return view('home');
+        $comics = Comic::all();
+        return view('home', compact($comics));
     }
 }
---
+++ database/seeds/ComicTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Comic;
+use Illuminate\Database\Seeder;
+
+class ComicTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics');
+        foreach ($comics as $item) {
+            $comic = new Comic();
+            $comic->title = $item['title'];
+            $comic->description = $item['description'];
+            $comic->thumb = $item['thumb'];
+            $comic->price = $item['price'];
+            $comic->sale_date = $item['sale_date'];
+            $comic->type = $item['type'];
+            $comic->save();
+        }
+    }
+}
